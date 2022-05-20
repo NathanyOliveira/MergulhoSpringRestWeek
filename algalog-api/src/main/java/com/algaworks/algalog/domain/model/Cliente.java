5c221cfcbd81d6a49b60cfe707e44ecd2207344e
Nathany <naty_om_3@hotmail.com>
2022-05-20 15:19:45 -0300

    Bean Validation

--- algalog-api/src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ algalog-api/src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -22,12 +25,19 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	
+	@NotBlank
+	@Size(max = 60 )
 	@Column // not necessary if it's the same name though
 	private String nome;
 	
-	
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
 	
+	@NotBlank
+	@Size(max = 20)
 	@Column(name = "fone")
 	private String telefone;
 	
